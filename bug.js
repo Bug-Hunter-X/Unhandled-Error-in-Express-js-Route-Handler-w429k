e61e2dc59dc4e32518c5d348a30f6eee368896fc
BugHunter-X <askzenai@gmail.com>
2025-01-25 22:08:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  // Missing error handling for invalid userId
+  // ...other code to fetch user data...
+  res.json({ userId });
+});
+app.listen(3000, () => console.log('Server is running'));
